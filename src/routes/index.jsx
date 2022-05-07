df915157d3a435769a2f5a6aa2ebf9cf14ccfdb8
Richard Cândido <rich_dacan@protonmail.com>
2022-05-07 09:59:26 -0300

    create: initial commit

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import { Switch } from 'react-router-dom'
+import Register from '../pages/Register'
 
 export default function Routes() {
   return (
     <Switch>
         <Route exact path='/'>
             <div>home</div>
+        </Route>
+        <Route path='/register'>
+          <Register/>
         </Route>
     </Switch>
   )
