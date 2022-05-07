737478587cd1b21a8d07badb97e150531cd5bf2c
Miguel Natan Tomaz <miguelnatantomaz@hotmail.com>
2022-05-07 09:57:58 -0300

    feat: login implemented h1

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-import { Switch } from 'react-router-dom'
+import React from "react";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import Login from "../pages/Login";
 
 export default function Routes() {
   return (
     <Switch>
-        <Route exact path='/'>
-            <div>home</div>
-        </Route>
+      <Route exact path="/">
+        <div>Home</div>
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
     </Switch>
-  )
+  );
 }
