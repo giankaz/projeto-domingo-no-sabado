28c513453c1d2088beb24907ef6a745320cd3aa8
Giankaz <91921192+Giankaz@users.noreply.github.com>
2022-05-07 09:59:14 -0300

    test

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,7 +5,7 @@ import { Switch } from 'react-router-dom'
 export default function Routes() {
   return (
     <Switch>
-        <Route exact path='/'>
+        <Route exact path='/' >
             <div>home</div>
         </Route>
     </Switch>
