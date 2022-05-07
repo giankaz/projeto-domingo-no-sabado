250a6619e54b6524c433c0c6cdf93beda747a188
Caio Giffoni <caio_cgf@hotmail.com>
2022-05-07 10:01:01 -0300

    :sparkles: feature created page cart

---
+++ src/pages/Carrinho/index.jsx
@@ -0,0 +1,11 @@
+export const Cart = () => {
+  /*const history = useHistory();
+
+  const { cart } = useCart();*/
+
+  return (
+    <div>
+      <h1>Carrinho - Caio</h1>
+    </div>
+  );
+};
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-import { Switch } from 'react-router-dom'
+import React from "react";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import { Cart } from "../pages/Carrinho";
 
 export default function Routes() {
   return (
     <Switch>
-        <Route exact path='/'>
-            <div>home</div>
-        </Route>
+      <Route exact path="/">
+        <div>home</div>
+      </Route>
+      <Route exact path="/carrinho">
+        <Cart />
+      </Route>
     </Switch>
-  )
+  );
 }
