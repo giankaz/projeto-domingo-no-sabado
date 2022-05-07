275c12f2df1d022fedc0ca29505632184a547e90
Paulo Vitor <pvitor7@hotmail.com>
2022-05-07 10:00:44 -0300

    home com button

---
+++ src/components /index.js
@@ -0,0 +1,18 @@
+import { useHistory } from "react-router-dom";
+
+const Home = () => {
+
+    const history = useHistory();
+
+    const handleClick = () => {
+        history.push("/login")
+    }
+
+    return(
+        <div>
+            <h1>Welcome</h1>
+
+            <button onClick={handleClick}>Login</button>
+        </div>
+    )
+}
