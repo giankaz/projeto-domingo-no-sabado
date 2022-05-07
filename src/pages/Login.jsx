ff919f76723e3b8b4410b21c313c3272cf8a82f1
Giankaz <91921192+Giankaz@users.noreply.github.com>
2022-05-07 18:11:25 -0300

    lol

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,59 @@
-
+import { useEffect, useState } from 'react';
+import { ReactMic } from '@cleandersonlobo/react-mic';
 import { Link } from "react-router-dom";
+import ReactPlayer from 'react-player';
+import ReactAudioPlayer from 'react-audio-player';
 
 const Login = () => {
+  const [record, setRecord] = useState(false)
+  const [audio, setAudio] = useState('') 
+  const [holder, setHolder] = useState([])
+  console.log(`🤖 ~ Login ~ holder`, holder)
+
+useEffect(() => {
+  setHolder([...holder, audio])
+}, [audio])
+
+  const start = () => setRecord(true)
+  const stop = () => setRecord(false)
+
+  const onStop = (recordedBlob) => {
+    setAudio(recordedBlob.blobURL)
+  }
+
+
   return (
     <>
       <h1>Login</h1>
       <Link to="/register" >
         Registre-se
       </Link>
+      {holder && holder.map((value) =>  {
+        if (value !== '') {
+          return ( 
+             <ReactAudioPlayer 
+             src={value}
+             controls
+             />
+
+             )
+            }
+      })}
+    
+        <ReactMic
+          record={record}
+          className="sound-wave"
+          onStop={onStop}
+
+          strokeColor="transparent"
+          backgroundColor="transparent" />
+        <button onClick={start} type="button">Start</button>
+        <button onClick={stop} type="button">Stop</button>
+      
+
+      <ReactPlayer 
+      url='https://www.youtube.com/watch?v=IKydZtF9I3w'
+      />
     </>
   );
 };
