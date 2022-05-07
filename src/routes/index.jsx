67545191d38a7793d40b2a6fb602ef19154309a6
Giankaz <91921192+Giankaz@users.noreply.github.com>
2022-05-07 09:50:16 -0300

    feat: product

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import { Switch } from 'react-router-dom'
+import ProductPage from '../pages/productPage'
 
 export default function Routes() {
+  
   return (
     <Switch>
         <Route exact path='/'>
             <div>home</div>
+        </Route>
+        <Route path='/productPage'>
+          <ProductPage/>
         </Route>
     </Switch>
   )
